c26fe408cf2e244a8656e89cdf0f3faeae7565c3
AllanPinheiro <142115513+AllanPinheiro@users.noreply.github.com>
2023-12-01 17:47:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const frm = document.querySelector("form")
+const btnsubmit = document.querySelector("#inSubmit")
+const result = document.querySelector("h3")
+
+btnsubmit.addEventListener("click", (e) => {
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+
+    const soma = (nota1 + nota2) / 2
+
+    
+    if (soma >= 7) {
+        result.innerText = `Nota ${soma.toFixed(1)} aprovado`
+        result.style.color = "green"
+    } else {
+        result.innerText = `Nota ${soma.toFixed(1)} reprovado`
+        result.style.color = "red"
+    }
+
+    e.preventDefault()
+})
